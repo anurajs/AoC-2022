8cb0ee102bae2b74f12dc23d3f047752bd7f90d2
Anuraj Tapidas <anutapid@publicisgroupe.net>
2022-12-15 17:48:23 +0000

    changed to one sort

--- day04/solution.py
+++ day04/solution.py
@@ -1,12 +1,21 @@
+from functools import cmp_to_key
 fileName = 'puzzle.txt'
 counter = 0
 counter2 = 0
+
+
+def compare_tuples(a, b):
+    if a[0] == b[0]:
+        return b[1] - a[1]
+    else:
+        return a[0] - b[0]
+
+
 with open(fileName) as file:
     while(line := file.readline()):
         pairs = list(map(lambda x: tuple(map(int, x)), map(
             lambda x: x.split('-'), line.split(','))))
-        pairs.sort(key=lambda x: x[1], reverse=True)
-        pairs.sort(key=lambda x: x[0])
+        pairs.sort(key=cmp_to_key(compare_tuples))
         if pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][1]:
             counter += 1
         if (pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][0]) or (pairs[0][1] >= pairs[1][1] and pairs[0][0] <= pairs[1][1]):
