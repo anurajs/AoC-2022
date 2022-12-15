950aeedc84d1b4b22f575790df0da73acb3f6c7c
Anuraj Tapidas <anutapid@publicisgroupe.net>
2022-12-15 17:56:01 +0000

    inlined the comparator function

--- day04/solution.py
+++ day04/solution.py
@@ -3,19 +3,12 @@ fileName = 'puzzle.txt'
 counter = 0
 counter2 = 0
 
-
-def compare_tuples(a, b):
-    if a[0] == b[0]:
-        return b[1] - a[1]
-    else:
-        return a[0] - b[0]
-
-
 with open(fileName) as file:
     while(line := file.readline()):
         pairs = list(map(lambda x: tuple(map(int, x)), map(
             lambda x: x.split('-'), line.split(','))))
-        pairs.sort(key=cmp_to_key(compare_tuples))
+        pairs.sort(key=cmp_to_key(
+            lambda a, b: b[1] - a[1] if a[0] == b[0] else a[0] - b[0]))
         if pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][1]:
             counter += 1
         if (pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][0]) or (pairs[0][1] >= pairs[1][1] and pairs[0][0] <= pairs[1][1]):
