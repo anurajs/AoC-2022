3c966fe1b5e9759ad9f81ab13d838dc8a50a68a5
Anuraj Tapidas <anutapid@publicisgroupe.net>
2022-12-16 01:21:38 +0000

    day 6 is a meme wtf

---
+++ day06/solution.py
@@ -0,0 +1,11 @@
+file_name = 'puzzle.txt'
+with open(file_name) as file:
+    line = file.readline()
+    for i in range(3, len(line)):
+        if(len(set(line[i-4:i])) == 4):
+            print('part 1', i)
+            break
+    for i in range(13, len(line)):
+        if(len(set(line[i-14:i])) == 14):
+            print('part 2', i)
+            break
